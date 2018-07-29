d231247ee0a21e40fa2a0955c4de3afc0bb1db0d
JeongHo <wwwkang8@gmail.com>
2018-07-29 14:38:18 +0900

    WebRestcontroller 작업.

--- src/test/java/com/jeongho/webservice/ApplicationTests.java
+++ src/test/java/com/jeongho/webservice/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
